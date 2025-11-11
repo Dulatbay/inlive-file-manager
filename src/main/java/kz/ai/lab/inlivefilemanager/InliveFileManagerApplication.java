eb311366959cf5ebc67529842ae8c9ff5c622036
Meiirzhan Muratbekuly <smilexah@inbox.ru>
2025-11-11 13:39:17 +0500

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/kz/ai/lab/inlivefilemanager/InliveFileManagerApplication.java
@@ -0,0 +1,13 @@
+package kz.ai.lab.inlivefilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InliveFileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InliveFileManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/ai/lab/inlivefilemanager/InliveFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ai.lab.inlivefilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InliveFileManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
